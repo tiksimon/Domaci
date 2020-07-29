9cd9211706cbabd73025ba8fdc1ab200341a570c
tijana <tijana.igrutinovic@rgf.rs>
2020-07-30 01:11:13 +0200

    Domaci za trecu nedelju

--- Domaci 2/Nedelja/zadatak3.js
+++ Domaci 2/Nedelja/zadatak3.js
@@ -2,6 +2,19 @@ let n = 5
 
 for(let i = 1; i <= n; i++) {
     let red = ""
+    for(let j = 1; j <= n; j++) {
+        if(j <= n - i){
+            red += " "
+        } else {
+            red += "#"
+        }
+    }
+    console.log(red)
+}
+
+
+for(let i = 1; i <= n; i++) {
+    let red = ""
     for(let j = 1; j <= i; j++) { 
             red += "#"
     }
